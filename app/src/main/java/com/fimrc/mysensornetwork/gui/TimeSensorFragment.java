eacd8fdead1573bb6716eb70ed19c66e33e90743
Sven Beckmann <sven.beckmann@fim-rc.de>
2017-03-21 20:06:07 +0100

    rebuild

--- app/src/main/java/com/fimrc/mysensornetwork/gui/TimeSensorFragment.java
+++ app/src/main/java/com/fimrc/mysensornetwork/gui/TimeSensorFragment.java
@@ -3,13 +3,11 @@ package com.fimrc.mysensornetwork.gui;
 
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.util.Pair;
 import android.support.v7.app.AlertDialog;
 import android.text.InputType;
 import android.util.Log;
